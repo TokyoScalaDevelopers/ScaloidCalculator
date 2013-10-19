0d3e47f6638a477f6604f39c1289b39881053d92
Devon Stewart <blast@hardchee.se>
2013-10-19 13:33:49 +0900

    Migrating identifiers and source files over

---
+++ src/com/meetup/TokyoScalaDevelopers/ScaloidCalculator.scala
@@ -0,0 +1,11 @@
+package com.meetup.TokyoScalaDevelopers
+
+import org.scaloid.common._
+import android.graphics.Color
+
+class HelloScaloid extends SActivity {
+  onCreate {
+    contentView = new SVerticalLayout {
+    }
+  }
+}
