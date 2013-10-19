4423d14fec10ade855a3a9cbd774cba63f75ab84
Devon Stewart <blast@hardchee.se>
2013-10-19 13:59:50 +0900

    Adding number buttons

--- src/com/meetup/TokyoScalaDevelopers/ScaloidCalculator.scala
+++ src/com/meetup/TokyoScalaDevelopers/ScaloidCalculator.scala
@@ -6,6 +6,17 @@ import android.graphics.Color
 class Calculator extends SActivity {
   onCreate {
     contentView = new SVerticalLayout {
+
+      for(row <- (1 to 9).grouped(3)) {
+        this += new SLinearLayout {
+          for(num <- row) {
+            SButton(s"$num")
+              .<<.Weight(1).>>
+              .onClick(toast(s"Clicked $num"))
+          }
+        }
+      }
+
     }
   }
 }
